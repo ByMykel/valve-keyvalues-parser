f5211c4a477955bc4abac8d21fcdada189cf0ab8
ByMykel <bymykel5@gmail.com>
2023-09-18 00:43:45 +0200

    chore: eslint, prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,15 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  extends: [
+    'plugin:vue/strongly-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
+    'prettier/prettier': 'error',
+    '@typescript-eslint/ban-ts-comment': 'off',
   },
 }
